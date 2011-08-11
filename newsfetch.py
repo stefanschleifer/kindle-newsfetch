ad78b34cb73d31f20c172d4aeb645d42cb57b982
Stefan Schleifer <stefan.schleifer@gmail.com>
2011-08-11 14:18:50 +0200

    method for creating configuraiton file added

--- newsfetch.py
+++ newsfetch.py
@@ -9,9 +9,10 @@
 #
 # (c) 2011 Stefan Schleifer, see LICENSE-file
 
-import sys
+import sys, os
 import ConfigParser
 
+# print help information
 def usage():
 	print "\nUsage: %s <command> [options]\n" % sys.argv[0]
 	print "\tall: Fetch and convert all configured items."
@@ -21,10 +22,41 @@ def usage():
 	print "\tlist: Get a list of all configured items."
 	sys.exit(1)
 
-def create_configfile():
-	pass
+# create configuraton file
+def create_configuration():
+	try:
+		i = raw_input("I'm going to ask you a few questions and create newsfetch.cfg in the current working directory (%s), is this ok (y/n)? " % os.getcwd())
+		if i is not 'y':
+			print "Ok, not creating newsfetch.cfg for you. Bye!"
+			sys.exit(1)
 
-def list_items():
+		config = ConfigParser.SafeConfigParser()
+		config.add_section('config')
+		config.set('config', 'KINDLE_ADDR', raw_input("Please enter your Kindle e-mail address where you want the converted files to be delivered to: "))
+		config.set('config', 'OUTPUT_PATH', raw_input("Please enter the absolute path to the directory for storing the converted files: "))
+		config.set('config', 'SMTP_SERVER', raw_input("Please enter the address of your desired SMTP server: "))
+		config.set('config', 'SMTP_USER', raw_input("Please enter the username for the given server: "))
+		config.set('config', 'SMTP_PW', raw_input("Please enter the password for the given user, WILL BE STORED IN PLAINTEXT: "))
+
+		config.add_section('example')
+		config.set('example', 'nytimes', 'New York Times')
+		config.set('example', 'sueddeutsche', 'Sueddeutsche Zeitung')
+
+		with open('newsfetch.cfg', 'w') as configfile:
+			config.write(configfile)
+
+	except Exception, e:
+		print "Could not create newsfetch.cfg: %s" % e
+	else:
+		print "Successfully created newsfetch.cfg. We've added example entries too."
+
+# list all configured items with their names
+def list_all_items():
+	print "not implemented yet"
+
+# return a list of unique recipe names which
+# should be converted in the current run
+def collect_recipes(section='all'):
 	print "not implemented yet"
 
 if '__main__' == __name__:
@@ -40,7 +72,7 @@ if '__main__' == __name__:
 	except:
 		i = raw_input("Neccessary file newsfetch.cfg could not be found, do you want to create it now (y/n)? ")
 		if 'y' == i:
-			create_configfile()
+			create_configuration()
 		else:
 			print "Cannot continue without configuration file. Either rerun %s and let it create newsfetch.cfg for you or create it manually. See example.cfg for possible options/values." % sys.argv[0]
 			sys.exit(1)
@@ -54,7 +86,7 @@ if '__main__' == __name__:
 	elif 'add' == sys.argv[1]:
 		pass
 	elif 'list' == sys.argv[1]:
-		list_items()
+		list_all_items()
 	else:
 		usage()
 
